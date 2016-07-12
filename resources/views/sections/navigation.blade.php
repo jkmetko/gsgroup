ec1973870d30b5b2d009af3144516aa67acccb09
3CKY-Kmetko <jakub.kmetko@3cky.sk>
2016-07-12 18:10:10 +0200

    bullets in slider

--- resources/views/sections/navigation.blade.php
+++ resources/views/sections/navigation.blade.php
@@ -25,7 +25,6 @@
                         <li><a class="scroll" href="#why-wisten5">Reklamné produkty</a></li>
                     </ul>
                 </li>
-                <li><a class="scroll" href="#history">Servis</a></li>
                 {{--<li class="dropdown-toggle nav-toggle"><a class="scroll" href="blog.html">blog <b data-toggle="dropdown" class="caret"></b></a>--}}
 
                     {{--<ul class="dropdown-menu">--}}
@@ -37,7 +36,7 @@
                     {{--</ul>--}}
 
                 {{--</li>--}}
-                <li><a class="scroll" href="#contact">contact</a></li>
+                <li><a class="scroll" href="#contact">Kontakt</a></li>
 
             </ul>
 
@@ -48,26 +47,23 @@
         <div class="dropdown mobile-drop">
             <a data-toggle="dropdown" class="mobile-menu" href="#"><i class="fa fa-bars"></i></a>
             <ul class="nav dropdown-menu fullwidth" role="menu" >
-                <li><a class="scroll" href="#home">home</a></li>
-                <li><a class="scroll" href="#about">about</a></li>
-                <li><a class="scroll" href="#features">features</a></li>
-                <li><a class="scroll" href="#history">history</a></li>
-                <li><a class="scroll" href="#team">team</a></li>
-                <li><a class="scroll" href="#portfolio">portfolio</a></li>
-                <li><a class="scroll" href="#services">services</a></li>
-                <li><a class="scroll" href="#prices">prices</a></li>
-                <li class="dropdown-toggle mobile-toggle"><a class="scroll">blog <b data-toggle="dropdown" class="caret"></b></a>
+                <li><a class="scroll" href="#home">Domov</a></li>
+                <li><a class="scroll" href="#services">O nás</a></li>
+                <li><a class="scroll" href="#team">Produkty</a></li>
+                <li class="dropdown-toggle mobile-toggle"><a class="scroll">Služby <b data-toggle="dropdown" class="caret"></b></a>
 
                     <ul class="dropdown-menu dr-mobile">
 
-                        <li><a href="blog.html">Blog Full Width</a></li>
-                        <li><a href="blog_01.html">With Sidebar Right</a></li>
-                        <li><a href="blog_02.html">With Sidebar Left</a></li>
+                        <li><a class="scroll" href="#why-wisten">Okamžité šetrenie</a></li>
+                        <li><a class="scroll" href="#why-wisten2">Starostlivosť</a></li>
+                        <li><a class="scroll" href="#why-wisten3">Ochutnávky</a></li>
+                        <li><a class="scroll" href="#why-wisten4">Servis zdarma</a></li>
+                        <li><a class="scroll" href="#why-wisten5">Reklamné produkty</a></li>
 
                     </ul>
 
                 </li>
-                <li><a class="scroll" href="#contact">contact</a></li>
+                <li><a class="scroll" href="#contact">Kontakt</a></li>
             </ul>
         </div>
 
